4b70079793de6b964a2039ab4a4e13199e2b90d5
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-07-28 12:25:08 +0200

    Removwe type annotations if unnecessary.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let query = &args[1];
+    let file_path = &args[2];
 
     println!("Searching for {query}");
     println!("In file {file_path}");

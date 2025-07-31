1ffe65839754f8d25d5feacc60c0c37ff4717ec6
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-07-31 13:59:32 +0200

    Use Array2 for matrices everywhere.

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,8 @@ fn main() -> Result<()> {
     }
 
     let params = fs::read_to_string("parameters.ron")?;
-    let mdl_par = MdlPar::new(&params).unwrap_or_else(|err| {
-        log::error!("failed to initialize model parameters: {:#?}", err);
+    let mdl_par = MdlPar::new(&params).unwrap_or_else(|error| {
+        log::error!("{:#?}", error);
         std::process::exit(1);
     });
 
--- src/params.rs
+++ src/params.rs
@@ -1,6 +1,5 @@
 use anyhow::{Context, Result, bail};
-use ndarray::Array2;
-use ron::de::from_str;
+use ndarray::{Array1, Array2, ArrayView1, ArrayView2};
 use serde::{Deserialize, Serialize};
 use std::fmt::{Debug, Display};
 use std::ops::RangeBounds;
@@ -21,34 +20,40 @@ pub struct MdlPar {
     pub std_dev_mut: f64,
 }
 
-fn _check_number<T, R>(val: T, name: &str, range: R) -> Result<()>
+fn _check_number<T, R>(value: T, name: &str, range: R) -> Result<()>
 where
     T: PartialOrd + Display,
     R: RangeBounds<T> + Debug,
 {
-    if !range.contains(&val) {
-        bail!("{name} must be in the range {:?}, but is {val}", range);
+    if !range.contains(&value) {
+        bail!("{name} must be in the range {:?}, but is {value}", range);
     }
     Ok(())
 }
 
 macro_rules! check_number {
-    ($val:expr, $range:expr) => {
-        _check_number($val, stringify!($val), $range)
+    ($value:expr, $range:expr) => {
+        _check_number($value, stringify!($value), $range)
     };
 }
 
-fn _check_vector(vec: &[f64], name: &str, exp_len: usize, check_prob: bool) -> Result<()> {
-    if vec.len() != exp_len {
-        bail!("{name} must have {exp_len} elements, but has {}", vec.len());
+fn _check_vector(
+    vector: ArrayView1<f64>,
+    name: &str,
+    expected_len: usize,
+    check_prob: bool,
+) -> Result<()> {
+    let len = vector.len();
+    if len != expected_len {
+        bail!("{name} must have length {expected_len}, but has {len}");
     }
     if !check_prob {
         return Ok(());
     }
-    if vec.iter().any(|&x| x < 0.0) {
+    if vector.iter().any(|&element| element < 0.0) {
         bail!("{name} must have only non-negative elements");
     }
-    let sum: f64 = vec.iter().sum();
+    let sum: f64 = vector.iter().sum();
     let tol = 1e-6;
     if (sum - 1.0).abs() > tol {
         bail!("{name} must sum to 1.0 (tolerance: {tol}), but sums to {sum}");
@@ -57,59 +62,66 @@ fn _check_vector(vec: &[f64], name: &str, exp_len: usize, check_prob: bool) -> R
 }
 
 macro_rules! check_vector {
-    ($vec:expr, $expected_len:expr, $check_prob:expr) => {
-        _check_vector(&$vec, stringify!($vec), $expected_len, $check_prob)
+    ($vector:expr, $expected_len:expr, $check_prob:expr) => {
+        _check_vector(&$vector, stringify!($vector), $expected_len, $check_prob)
     };
 }
 
 fn _check_matrix(
-    mat: &[Vec<f64>],
+    matrix: ArrayView2<f64>,
     name: &str,
-    exp_size: (usize, usize),
+    expected_shape: &[usize],
     check_trans: bool,
 ) -> Result<()> {
-    let (exp_rows, exp_cols) = exp_size;
-    if mat.len() != exp_rows {
-        bail!("{name} must have {exp_rows} rows, but has {}", mat.len());
-    }
-    for (i, row) in mat.iter().enumerate() {
-        if row.len() != exp_cols {
-            bail!(
-                "{name} row {} length must be {}, but is {}",
-                i,
-                exp_cols,
-                row.len()
-            );
-        }
+    let shape = matrix.shape();
+    if shape != expected_shape {
+        bail!(
+            "{name} must have shape {:?}, but has {:?}",
+            expected_shape,
+            shape
+        );
     }
     if !check_trans {
         return Ok(());
     }
-    if exp_rows != exp_cols {
+    if shape[0] != shape[1] {
         bail!("{name} is not a square matrix");
     }
-    for (i, row) in mat.iter().enumerate() {
-        _check_vector(row, &format!("{} row {}", name, i), exp_cols, true)?;
+    for (i_row, row) in matrix.outer_iter().enumerate() {
+        _check_vector(row, &format!("{name} row {i_row}"), shape[1], true)?;
     }
     Ok(())
 }
 
 macro_rules! check_matrix {
-    ($matrix:expr, $expected_size:expr, $check_trans:expr) => {
-        _check_matrix(&$matrix, stringify!($matrix), $expected_size, $check_trans)
+    ($matrix:expr, $expected_shape:expr, $check_trans:expr) => {
+        _check_matrix($matrix, stringify!($matrix), $expected_shape, $check_trans)
     };
 }
 
 impl MdlPar {
     pub fn new(params: &str) -> Result<Self> {
-        let mdl_par: MdlPar = from_str(params).context("failed to deserialize MdlPar")?;
+        let mdl_par: MdlPar =
+            ron::de::from_str(params).context("failed to deserialize MdlPar value from string")?;
 
         check_number!(mdl_par.n_env, 1..100)?;
         check_number!(mdl_par.n_phe, 1..100)?;
 
-        // check_matrix!(mdl_par.prob_env, (mdl_par.n_env, mdl_par.n_env), true)?;
-        // check_matrix!(mdl_par.prob_rep, (mdl_par.n_phe, mdl_par.n_env), false)?;
-        // check_matrix!(mdl_par.prob_dec, (mdl_par.n_phe, mdl_par.n_env), false)?;
+        check_matrix!(
+            mdl_par.prob_env.view(),
+            &[mdl_par.n_env, mdl_par.n_env],
+            true
+        )?;
+        check_matrix!(
+            mdl_par.prob_rep.view(),
+            &[mdl_par.n_phe, mdl_par.n_env],
+            false
+        )?;
+        check_matrix!(
+            mdl_par.prob_dec.view(),
+            &[mdl_par.n_phe, mdl_par.n_env],
+            false
+        )?;
 
         check_number!(mdl_par.n_agt_init, 1..10_000)?;
         check_number!(mdl_par.std_dev_mut, 0.0..1.0)?;
